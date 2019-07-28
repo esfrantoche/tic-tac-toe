e5f230cd4e59e64d42077279424a264273373e90
Francisco Toche Hernandez <Rodolfo.Toche_Hernandez@telus.com>
2019-07-28 17:12:11 -0600

    Basic UI(clasic 3x3), turn-based functionality, handle occupied square

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor (props){
+    super(props)
+    this.state = {
+      board : Array(9).fill(null),
+      turn: "X",
+      winner: null
+    }
+  }
+  handleClick(index){
+    let updatedBoard = this.state.board;
+    if (this.state.board[index] === null && !this.state.winner){
+      updatedBoard[index] = this.state.turn;
+      let nextTurn = this.state.turn === "X" ? "0" : "X";
+      this.setState({
+        board: updatedBoard,
+        turn: nextTurn
+      });
+    }
+    this.winCalculation();
+  }
+  winCalculation(){
+    let tttRows =
+    [
+      ["0", "1", "2"],
+      ["3", "4", "5"],
+      ["6", "7", "8"],
+      ["0", "3", "6"],
+      ["1", "4", "7"],
+      ["2", "5", "8"],
+      ["0", "4", "8"],
+      ["2", "4", "6"],
+    ]
+    for (let index = 0; index < tttRows.length; index++) {
+      const [a,b,c] = tttRows[index];
+      if(this.state.board[a] && this.state.board[a] === this.state.board[b] && this.state.board[a] === this.state.board[c]){
+        alert("You won");
+        this.setState({winner:this.state.turn});
+      }
+    }
+  }
+  render(){
+    const Box = this.state.board.map((box, index) => 
+                                          <div
+                                              className="box col-md-4 text-center" 
+                                              key={index} 
+                                              onClick={ (e) => this.handleClick(index)}>
+                                                {box}
+                                          </div>);
+    return (
+      <div className="container">
+        <h1>Tic Tac Toe Game</h1>
+        <div className="row">
+          {Box}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
